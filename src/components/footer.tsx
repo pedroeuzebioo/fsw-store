c54403b7c37e8beb81394962d64bc9764bb2c6bf
Pedro Henrique <eupedrohenrique.euzebio@gmail.com>
2023-11-17 15:22:57 -0300

    feat: align text footer center

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 text-center">
       © 2023 Copyright Pedro Henrique |{" "}
       <span className="font-semibold">FSW Store</span>
     </div>
