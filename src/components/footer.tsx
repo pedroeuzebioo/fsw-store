62a3ae356ebef8de0cd00e72f7b281fb9e65b63d
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-01-09 08:25:47 -0300

    style(footer): improve footer styling for better responsiveness

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,10 @@
 const Footer = () => {
   return (
-    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 text-center">
-      © 2023 Copyright Pedro Henrique |{" "}
-      <span className="font-semibold">FSW Store</span>
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
+      <div className="mx-auto lg:container">
+        © 2023 Copyright Pedro Euzebio{" "}
+        <span className="font-semibold">FSW Store</span>
+      </div>
     </div>
   );
 };
